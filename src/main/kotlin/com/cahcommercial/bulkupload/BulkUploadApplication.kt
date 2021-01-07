5bac4d89611fa82db14006b3819c617347611f26
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2021-01-07 09:09:37 -0500

    initial commit

---
+++ src/main/kotlin/com/cahcommercial/bulkupload/BulkUploadApplication.kt
@@ -0,0 +1,11 @@
+package com.cahcommercial.bulkupload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BulkUploadApplication
+
+fun main(args: Array<String>) {
+	runApplication<BulkUploadApplication>(*args)
+}
---
+++ src/test/kotlin/com/cahcommercial/bulkupload/BulkUploadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cahcommercial.bulkupload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BulkUploadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
