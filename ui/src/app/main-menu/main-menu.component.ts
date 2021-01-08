585ae5002ebc27c8ef96ba28cbb18d3d6a2ef650
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2021-01-08 08:27:40 -0500

    parse xlsx

--- ui/src/app/main-menu/main-menu.component.ts
+++ ui/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-main-menu',
@@ -8,13 +9,43 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MainMenuComponent implements OnInit {
 
   constructor() { }
-  invoice = 'test'
+  invoice = '123'
+  excelFile: any
 
   ngOnInit(): void {
   }
 
-  onClickMe(){
-    console.log("clicked" + this.invoice)
+  upload(){
+    console.log("clicked " + this.invoice)
+    this.parseFile()
   }
+
+  onFileChange(event: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(event.target);
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
+    }
+    this.excelFile = target.files[0]
+  }
+
+  parseFile() {
+    console.log(this.excelFile)
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(this.excelFile);
+    reader.onload = (e: any) => {
+      /* create workbook */
+      const binarystr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+
+      /* selected the first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      const data = XLSX.utils.sheet_to_json(ws); // to get 2d array pass 2nd parameter as object {header: 1}
+      console.log(data); // Data will be logged in array format containing objects
+    };
+ }
 
 }
