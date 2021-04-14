79a2eab8069190c872cac4d282c09636cc5a18e5
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2021-04-14 11:07:34 -0400

    reasons first pass

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ReasonsComponent } from './reasons/reasons.component';
 
 const routes: Routes = [
-  { path: '', component: MainMenuComponent }
+  { path: '', component: MainMenuComponent },
+  { path: 'reasons', component: ReasonsComponent }
 ];
 
 @NgModule({
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { FormsModule } from '@angular/forms';
+import { ReasonsComponent } from './reasons/reasons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    ReasonsComponent
   ],
   imports: [
     BrowserModule,
